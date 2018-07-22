788a06a4e7014d6a5f21476b3704dd3da5cee0e9
a21271 <a21271@aliyun.com>
2018-07-22 15:50:46 +0800

    add login background picture

--- webui/src/modules/index/views/Login.vue
+++ webui/src/modules/index/views/Login.vue
@@ -1,20 +1,38 @@
 <template>
-    <el-form :model="ruleForm2" :rules="rules2" ref="ruleForm2" label-position="left" label-width="0px"
-             class="demo-ruleForm login-container">
-        <h3 class="title">系统登录</h3>
-        <el-form-item prop="account">
-            <el-input type="text" v-model="ruleForm2.account" auto-complete="off" placeholder="账号"></el-input>
-        </el-form-item>
-        <el-form-item prop="checkPass">
-            <el-input type="password" v-model="ruleForm2.checkPass" auto-complete="off" placeholder="密码"></el-input>
-        </el-form-item>
-        <el-checkbox v-model="checked" checked class="remember">记住密码</el-checkbox>
-        <el-form-item style="width:100%;">
-            <el-button type="primary" style="width:100%;" @click.native.prevent="handleSubmit2" :loading="logining">登录
-            </el-button>
-            <!--<el-button @click.native.prevent="handleReset2">重置</el-button>-->
-        </el-form-item>
-    </el-form>
+    <el-row class="container login-background">
+        <el-form :model="ruleForm2" :rules="rules2" ref="ruleForm2" label-position="left" label-width="0px"
+                 class="demo-ruleForm login-container">
+            <h3 class="title">系统登录</h3>
+            <el-form-item prop="account">
+                <el-input type="text" v-model="ruleForm2.account" auto-complete="off" placeholder="账号"></el-input>
+            </el-form-item>
+            <el-form-item prop="checkPass">
+                <el-input type="password" v-model="ruleForm2.checkPass" auto-complete="off" placeholder="密码"></el-input>
+            </el-form-item>
+            <el-checkbox v-model="checked" checked class="remember">记住密码</el-checkbox>
+            <el-form-item style="width:100%;">
+                <el-button type="primary" style="width:100%;" @click.native.prevent="handleSubmit2" :loading="logining">登录
+                </el-button>
+                <!--<el-button @click.native.prevent="handleReset2">重置</el-button>-->
+            </el-form-item>
+        </el-form>
+    </el-row>
+    <!--<el-form :model="ruleForm2" :rules="rules2" ref="ruleForm2" label-position="left" label-width="0px"-->
+             <!--class="demo-ruleForm login-container">-->
+        <!--<h3 class="title">系统登录</h3>-->
+        <!--<el-form-item prop="account">-->
+            <!--<el-input type="text" v-model="ruleForm2.account" auto-complete="off" placeholder="账号"></el-input>-->
+        <!--</el-form-item>-->
+        <!--<el-form-item prop="checkPass">-->
+            <!--<el-input type="password" v-model="ruleForm2.checkPass" auto-complete="off" placeholder="密码"></el-input>-->
+        <!--</el-form-item>-->
+        <!--<el-checkbox v-model="checked" checked class="remember">记住密码</el-checkbox>-->
+        <!--<el-form-item style="width:100%;">-->
+            <!--<el-button type="primary" style="width:100%;" @click.native.prevent="handleSubmit2" :loading="logining">登录-->
+            <!--</el-button>-->
+            <!--&lt;!&ndash;<el-button @click.native.prevent="handleReset2">重置</el-button>&ndash;&gt;-->
+        <!--</el-form-item>-->
+    <!--</el-form>-->
 </template>
 
 <script>
@@ -77,6 +95,12 @@
 </script>
 
 <style lang="scss" scoped>
+    .login-background {
+        background: url(~assets/images/background.jpg) no-repeat top center;
+        background-size: 100%;
+        overflow: hidden;
+    }
+
     .login-container {
         /*box-shadow: 0 0px 8px 0 rgba(0, 0, 0, 0.06), 0 1px 0px 0 rgba(0, 0, 0, 0.02);*/
         -webkit-border-radius: 5px;
